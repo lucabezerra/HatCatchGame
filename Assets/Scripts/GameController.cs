b4f94d52e694e59f3f8e93e053d0a75adbc0262f
Luca Bezerra <lucabezerra@gmail.com>
2016-09-16 16:02:30 -0300

    Initial commit. Full game developed.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public Camera cam;
+    public GameObject[] balls;
+    public float timeLeft;
+    public Text timerText;
+    public Text scoreText;
+    public GameObject gameOverText;
+    public GameObject restartButton;
+    public GameObject splashScreen;
+    public GameObject startButton;
+
+    public HatController hatController;
+
+    private float maxWidth;
+    private bool isPlaying;
+
+    // Use this for initialization
+    void Start()
+    {        
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }
+
+        Vector3 upperCorner = new Vector3(Screen.width, Screen.height, 0);
+        Vector3 targetWidth = cam.ScreenToWorldPoint(upperCorner);
+        float ballWidth = balls[0].GetComponent<SpriteRenderer>().bounds.extents.x;
+        maxWidth = targetWidth.x - ballWidth;
+
+        timerText.text = "Time Left:\n" + timeLeft;
+        isPlaying = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (isPlaying)
+        {
+            timeLeft -= Time.deltaTime;
+
+            if (timeLeft >= 0f)
+            {
+                timerText.text = "Time Left:\n" + Mathf.RoundToInt(timeLeft).ToString();
+            }
+            else
+            {
+                timerText.text = "Time Left:\n00";
+            }
+        }
+    }
+
+    public void StartGame()
+    {
+        splashScreen.SetActive(false);
+        startButton.SetActive(false);
+        timerText.gameObject.SetActive(true);
+        scoreText.gameObject.SetActive(true);
+        hatController.ToggleControl();
+        StartCoroutine("Spawn");
+    }
+
+    IEnumerator Spawn() {
+        yield return new WaitForSeconds(2f);
+        isPlaying = true;
+
+        while (timeLeft > 0)
+        {
+            GameObject ball = balls[Random.Range(0, balls.Length)];
+            Vector3 spawnPosition = new Vector3(Random.Range(-maxWidth, maxWidth), 
+                                    this.transform.position.y, 
+                                    0);
+            Instantiate(ball, spawnPosition, Quaternion.identity);
+
+            yield return new WaitForSeconds(Random.Range(1f, 2f));
+        }
+
+        yield return new WaitForSeconds(2f);
+        gameOverText.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        restartButton.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/HatController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HatController : MonoBehaviour
+{
+
+    public Camera cam;
+
+    private bool canControl;
+    private float maxWidth;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }
+
+        Vector3 upperCorner = new Vector3(Screen.width, Screen.height, 0);
+        Vector3 targetWidth = cam.ScreenToWorldPoint(upperCorner);
+        float hatWidth = this.GetComponent<Renderer>().bounds.extents.x;
+        maxWidth = targetWidth.x - hatWidth;
+
+        canControl = false;
+    }
+	
+    // Update is called once per physics timestep
+    void FixedUpdate()
+    {
+        if (canControl)
+        {
+            Vector3 rawPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 targetPosition = new Vector3(rawPosition.x, 0, 0);
+            float targetWidth = Mathf.Clamp(targetPosition.x, -maxWidth, maxWidth);
+            targetPosition = new Vector3(targetWidth, targetPosition.y, targetPosition.z);
+            this.GetComponent<Rigidbody2D>().MovePosition(targetPosition);
+        }
+    }
+
+    public void ToggleControl()
+    {
+        canControl = !canControl;
+    }
+}
